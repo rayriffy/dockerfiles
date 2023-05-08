ae098b96090ed4eadb2cf572d8eb8d202f76b7ff
Phumrapee Limpianchop <git@rayriffy.com>
2023-05-08 09:02:03 +0900

    feat: initial c

---
+++ hentaiathome/build/start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+kill_jar() {
+  echo 'Received TERM'
+  pkill java
+  wait "$(ps -ef | pgrep java)"
+  echo 'Process finished'
+}
+
+if [ $HatH_KEY ]
+	then
+		echo -n "${HatH_KEY}" > /hath/data/data/client_login
+	else
+		if [ ! -f /hath/data/data/client_login ]; then
+		echo "Login not found, try specify the HatH_KEY arg, exiting......"
+		exit 1
+		fi
+fi
+
+trap 'kill_jar' TERM INT KILL
+java -jar HentaiAtHome.jar $HatH_ARGS  &
+
+wait $!
